2408b445c93d1705dc0f6c7c13733fa5105b24de
barmao <bryanbarmao@gmail.com>
2022-12-20 13:38:01 +0300

    added configurations

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+require("dotenv").config();
+require("./config/database").connect();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
